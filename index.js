d38b833831d16e96b0bf4cbc87ae1948a2f1cdf2
stardom1124 <stardom1124>
2020-05-19 22:45:28 +0900

    bot upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+const welcomeChannelName = "welcome";
+
+const welcomeChannelComment = "안녕하세요 HKSR 서버에 오신것을 환영합니다.";
+
+
+client.on('ready', () => {
+  console.log('켰다.');
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+
+  member.addRole(guild.roles.find(role => role.name == "GUEST"));
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
+client.on('message', (message) => {
+  if(message.author.bot) return;
+
+  if(message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(token);
